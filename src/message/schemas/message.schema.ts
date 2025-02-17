41c925e6b3cf59b8d28cbbf7d0d25a4ac3aac3e1
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2025-02-17 09:50:37 +0330

    ADD message module, controller, service

---
+++ src/message/schemas/message.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type MessageDocument = Message & Document;
+
+@Schema()
+export class Message {
+  @Prop({ required: true })
+  sender: string;
+  @Prop({ required: true })
+  conversation: string;
+  @Prop({ required: true })
+  content: string;
+  @Prop({ default: Date.now })
+  createdAt: Date;
+  @Prop()
+  attachments?: string[];
+}
