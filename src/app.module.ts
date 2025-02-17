c9848af802c54a2325db1e778f0b04cc9bf97cd5
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2025-02-17 09:16:19 +0330

    ADD auth module, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING as string, {}),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
