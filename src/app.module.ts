46e95f41a98b5cb7a91e2235f7e6814e1957c92e
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2025-02-17 09:50:30 +0330

    ADD message module, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ConversationModule } from './conversation/conversation.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConversationModule } from './conversation/conversation.module';
     MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING as string, {}),
     AuthModule,
     ConversationModule,
+    MessageModule,
   ],
   controllers: [AppController],
   providers: [AppService],
